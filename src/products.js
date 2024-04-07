c525f66920f4ecdaf8d6c860d40084505cd751f5
Test <kia283@ya.ru>
2024-04-07 23:22:50 +1000

    change products.js

--- src/products.js
+++ src/products.js
@@ -1,33 +1,40 @@
+import img1 from './img/1.jpg';
+import img2 from './img/2.jpg';
+import img3 from './img/3.jpg';
+import img4 from './img/4.jpg';
+import img5 from './img/5.jpg';
+import img7 from './img/7.jpg';
+
 const products = [{
     name: "Nike Metcon 2",
     price: "130",
     color: "red",
-    img: "./src/img/1.jpg"
+    img: img1,
 }, {
     name: "Nike Metcon 2",
     price: "130",
     color: "green",
-    img: "./src/img/2.jpg"
+    img: img2,
 }, {
     name: "Nike Metcon 2",
     price: "130",
     color: "blue",
-    img: "./src/img/3.jpg"
+    img: img3,
 }, {
     name: "Nike Metcon 2",
     price: "130",
     color: "black",
-    img: "./src/img/4.jpg"
+    img: img4,
 }, {
     name: "Nike free run",
     price: "170",
     color: "black",
-    img: "./src/img/7.jpg"
+    img: img5,
 }, {
     name: "Nike Metcon 3",
     price: "150",
     color: "green",
-    img: "./src/img/5.jpg"
+    img: img7
 }];
 
 export default products;
